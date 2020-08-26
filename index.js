e5257fd7ba65447e6046b65795b12119754c50a0
Anton Verinov <git@zemlan.in>
2020-08-26 10:35:14 +0300

    floor -> ceil

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const now = new Date();
   const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][now.getUTCDay()];
-  const day = Math.floor((now - new Date(2020, 2, 1, 0, 0, 0)) / (24 * 60 * 60 * 1000));
+  const day = Math.ceil((now - new Date(2020, 2, 1, 0, 0, 0)) / (24 * 60 * 60 * 1000));
   res.write(`<title>Wake Me Up When March Ends</title><h1>Today is ${dayOfWeek}, March ${day}, 2020</h1>`);
 
   res.writeHead(200, {
