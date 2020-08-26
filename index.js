0fc69b00ea8142c394a5aa1039ca655c627bef97
Anton Verinov <git@zemlan.in>
2020-08-26 10:42:07 +0300

    ordinal suffix

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 module.exports = (req, res) => {
   const now = new Date();
-  const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][now.getUTCDay()];
-  const day = Math.ceil((now - new Date(2020, 2, 1, 0, 0, 0)) / (24 * 60 * 60 * 1000));
-  res.write(`<title>Wake Me Up When March Ends</title><h1>Today is ${dayOfWeek}, March ${day}, 2020</h1>`);
+  const dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ][now.getUTCDay()];
+  const day = Math.ceil(
+    (now - new Date(2020, 2, 1, 0, 0, 0)) / (24 * 60 * 60 * 1000)
+  );
+  const suffix = {
+    one: "st",
+    two: "nd",
+    few: "rd",
+    other: "th",
+  }[new Intl.PluralRules("en", { type: "ordinal" }).select(day)];
+  res.write(
+    `<title>Wake Me Up When March Ends</title><h1>Today is ${dayOfWeek}, March ${day}<sup>${suffix}</sup>, 2020</h1>`
+  );
 
   res.writeHead(200, {
     "Content-Type": "text/html",
-    "Cache-Control": `s-maxage=${60 * 60}, max-age=0`
+    "Cache-Control": `s-maxage=${60 * 60}, max-age=0`,
   });
   res.end();
 };
